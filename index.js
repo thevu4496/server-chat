ea72db57acca209fa299a03024b05c12fb2e29e2
thevu4496 <ts2a1996@gmail.com>
2021-02-16 23:36:35 +0700

    xxxx

--- index.js
+++ index.js
@@ -45,6 +45,3 @@ io.on('connection', function(socket) {
 app.get('/', (req, res) => {
     res.send("Home page. Server running okay.");
 })
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
